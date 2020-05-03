a4b3e5e5933d36c72b7aa8e2cad0ea83f240fd3d
Balram <singhbalram054@gmail.com>
2020-05-03 14:12:30 +0530

    intial commit

---
+++ c++/syntax.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int f(){
+	cout<<"full"<<endl;
+	return 3;
+}
+
+int main(int argc, char const *argv[])
+{
+	cout<<"Hello World";
+
+	cout<<endl;
+cout<<f();
+	return 0;
+
+}
